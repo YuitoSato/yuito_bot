1ff45bddd7356394f5876f6b4f3817e7b49f1363
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-02-27 23:30:32 +0900

    delete articles tabel

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221055218) do
-
-  create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "title"
-    t.string   "url"
-    t.text     "content",      limit: 65535
-    t.time     "published_at"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
