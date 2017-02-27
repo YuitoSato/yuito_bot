e7e2ddbf9a2867b461297eec0c3ae5fafa9d9787
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-02-27 15:06:25 +0900

    refactor webhook_controller

--- app/controllers/api/webhook_controller.rb
+++ app/controllers/api/webhook_controller.rb
@@ -4,31 +4,12 @@ class Api::WebhookController < ApplicationController
   def callback
     body = request.raw_post
     client ||= LineBotClient.new
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
 
-    unless client.validate_signature(body, signature)
+    unless client.validate_signature(body, request.env['HTTP_X_LINE_SIGNATURE'])
       render body: nil, status: 470 and return
     end
 
-    events = client.parse_events_from(body)
-
-    events.each { |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text'] + 'っすね'
-          }
-          client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = Tempfile.open("content")
-          tf.write(response.body)
-        end
-      end
-    }
+    client.response(body)
 
     render body: nil, status: :ok
   end
--- app/services/line_bot_client.rb
+++ app/services/line_bot_client.rb
@@ -3,4 +3,20 @@ class LineBotClient < Line::Bot::Client
     @channel_token  = ENV['LINE_CHANNEL_TOKEN']
     @channel_secret = ENV['LINE_CHANNEL_SECRET']
   end
+
+  def response(body)
+    parse_events_from(body).each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text'] + 'っすね'
+          }
+          reply_message(event['replyToken'], message)
+        end
+      end
+    end
+  end
 end
