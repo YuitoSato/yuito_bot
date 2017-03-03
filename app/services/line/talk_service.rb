f5e955945b0da179763864813df4550da944d12c
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-03-03 23:50:49 +0900

    refactor

--- app/services/line/talk_service.rb
+++ app/services/line/talk_service.rb
@@ -14,14 +14,14 @@ module Line
           .try(:fetch, 'Keyphrase') ||
         YahooMAService.new(text)
           .execute
-          .select{|word| word["pos"] == "形容詞" ||
-            word["pos"] == "感動詞" ||
-            word["pos"] == "副詞"}
+          .select{|word| word['pos'] == '形容詞' ||
+            word['pos'] == '感動詞' ||
+            word['pos'] == '副詞'}
           .first
           .try(:fetch, 'surface')
 
       text      = keyphrase ? keyphrase + 'っすね' : 'ちょっと何言ってるか分からないっすw'
-      text.insert(0, "つまり") if text.length > 26
+      text.insert(0, 'つまり') if text.length > 26
 
       {
         type: 'text',
