93a682e45e929351e51811dbb40aa87f5f307cbb
佐藤有斗 <satoyuito@satoyuito-MacBook-Pro.local>
2017-03-03 23:41:48 +0900

    add two services

--- app/services/line_bot_client.rb
+++ app/services/line_bot_client.rb
@@ -11,40 +11,21 @@ class LineBotClient < Line::Bot::Client
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          # keyphrase =
-          #   YahooKeyphraseService.new(event.message['text']).execute.try(:first).try(:fetch, 'Keyphrase') ||
-          #   YahooMAService.new(event.message['text']).execute.select{|word| word["pos"] == "形容詞" || word["pos"] == "感動詞" || word["pos"] == "副詞"}.first.try(:fetch, 'surface')
-          # text      = keyphrase ? keyphrase + 'っすね' : 'ちょっと何言ってるか分からないっすw'
-          # text.insert(0, "つまり") if event.message['text'].length > 26
-          res = GoogleCustomSearchApi.search(event.message['text']).items.try(:first)
-          image = res.pagemap.cse_thumbnail.try(:first).try(:fetch, 'src') if res['cse_thumbnail']
-          # text = res.try(:fetch, 'title') + '\\n' + res.try(:fetch, 'link')
-          message = {
-            "type": "template",
-            "altText": "this is a buttons template",
-            "template": {
-                "type": "buttons",
-                "thumbnailImageUrl": image,
-                "title": res.title,
-                "text": res.snippet[0, 60],
-                "actions": [
-                    {
-                      "type": "uri",
-                      "label": "Click here",
-                      "uri": res.link
-                    }
-                ]
-            }
-          }
-
+          text = event.message['text']
+          message = case user.mode
+          when 'talk'
+            Line::TalkService.new(text).execute
+          when 'search'
+            Line::SearchService.new(text).execute
+          end
           response = reply_message(event['replyToken'], message)
+
           if response.instance_of?(Net::HTTPBadRequest)
-            binding.pry
             message = {
               type: 'text',
               text: 'エラーしたよ'
             }
-            reply_message(event['replyToken'], message)
+            push_message(user.line_id, message)
           end
         end
       end
